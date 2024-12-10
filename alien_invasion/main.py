e091748ce4ddf6a24f28cb94e7cca5efef43b90d
Mjh <miao.jiahao@foxmail.com>
2024-12-11 00:51:52 +0800

    [新增] 外星人

--- alien_invasion/main.py
+++ alien_invasion/main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 
 import game_functions as gf
+from alien import Alien
 from settings import Settings
 from ship import Ship
 
@@ -16,8 +17,14 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 设置背景色
     # bg_color = (230, 230, 230)
@@ -28,7 +35,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
         # 每次循环都重绘屏幕
         # screen.fill(bg_color)
