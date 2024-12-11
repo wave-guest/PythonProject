3dc881710ad589c28c47ed03ba5aaeeb9c523954
Mjh <miao.jiahao@foxmail.com>
2024-12-12 00:40:42 +0800

    [新增] 边界状态 启动状态

--- alien_invasion/main.py
+++ alien_invasion/main.py
@@ -37,9 +37,11 @@ def run_game():
     while True:
         # 监视键盘和鼠标事件
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
         # 每次循环都重绘屏幕
