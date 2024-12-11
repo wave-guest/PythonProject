a15122fb6d74f4140212c864c1ff75d2a15d142e
Mjh <miao.jiahao@foxmail.com>
2024-12-11 23:37:08 +0800

    [新增] 子弹击杀外星人

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -61,7 +61,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """"更新子弹的位置， 并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
@@ -71,6 +71,9 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
     print(len(bullets))
+    # 检查是否有子弹击中了外星人
+    # 如果是这样， 就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def create_fleet(ai_settings, screen, ship, aliens):
