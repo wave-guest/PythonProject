9a0663de15d1c96f24a988f88f6e7058cd7db1ce
Mjh <miao.jiahao@foxmail.com>
2024-12-12 22:35:43 +0800

    [新增] 开始按钮

--- alien_invasion/main.py
+++ alien_invasion/main.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 import game_functions as gf
 from alien import Alien
+from button import Button
 from game_stats import GameStats
 from settings import Settings
 from ship import Ship
@@ -15,6 +16,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建play按钮
+    play_button = Button(ai_settings, screen, "Play")
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
@@ -36,13 +40,13 @@ def run_game():
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
         # 每次循环都重绘屏幕
         # screen.fill(bg_color)
