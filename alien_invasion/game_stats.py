b5348283b3f7b5f2027a9bc14b208d05e4c5710d
Mjh <miao.jiahao@foxmail.com>
2024-12-13 23:45:25 +0800

    [新增] 完整计分系统

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -11,6 +11,7 @@ class GameStats:
 
         # 在任何情况下都不应该重置最高得分
         self.high_score = 0
+        self.level = 1
 
     def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
--- alien_invasion/main.py
+++ alien_invasion/main.py
@@ -42,11 +42,11 @@ def run_game():
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
         gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
